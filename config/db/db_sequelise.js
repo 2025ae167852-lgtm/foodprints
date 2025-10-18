2d5dfbcb644a0c714fa6e7b9bd98f2a3a73eaefb
2025ae167852-lgtm <2025ae167852@kstvet.ac.ke>
2025-10-18 13:32:56 +0300

    Update db_sequelise.js

--- config/db/db_sequelise.js
+++ config/db/db_sequelise.js
@@ -1,20 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 const connectionString = process.env.DATABASE_URL;
-const sequelize = new Sequelize(connectionString, {
-  dialect: process.env.DB_DIALECT || 'postgres',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
   protocol: 'postgres',
-  logging: false,
   dialectOptions: {
-    ssl: process.env.DB_SSL === 'true'
-      ? {
-          require: true,
-          rejectUnauthorized: false, // Necessary for Render
-        }
-      : false,
-  },
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
-
 sequelize
   .authenticate()
   .then(() => {
